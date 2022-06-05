02fe69e9abe6746265bbe441b56c51c0155b6770
Anshul Verma <itsanshulverma@gmail.com>
2022-06-06 01:38:27 +0530

    Fixed spaces

--- 1_DataClickEventsTemplates/app.js
+++ 1_DataClickEventsTemplates/app.js
@@ -1,21 +1,21 @@
 const app = Vue.createApp({
-	// data, functions, template
-	// template: '<h2>Hello Vue! :)</h2>',
-	data() {
-		return{
-			title: "Harry Potter: Prisoner of Azkaban",
-			author: "J.K. Rowling",
-			counter: 0
-		}
-	},
-	methods: {
-		changeTitle() {
-			this.title = "Harry Potter: Chamber of Secrets";
-		},
-		changeTitle2(title) {
-			this.title = title;
-		}
-	}
+  // data, functions, template
+  // template: '<h2>Hello Vue! :)</h2>',
+  data() {
+    return {
+      title: "Harry Potter: Prisoner of Azkaban",
+      author: "J.K. Rowling",
+      counter: 0
+    }
+  },
+  methods: {
+    changeTitle() {
+      this.title = "Harry Potter: Chamber of Secrets";
+    },
+    changeTitle2(title) {
+      this.title = title;
+    }
+  }
 })
 
 app.mount('#app')
--- 2_CondititonalRendering/app.js
+++ 2_CondititonalRendering/app.js
@@ -1,16 +1,16 @@
 const app = Vue.createApp({
-	data() {
-		return{
-			showBooks: true,
-			title: "Harry Potter: Prisoner of Azkaban",
-			author: "J.K. Rowling"
-		}
-	},
-	methods: {
-		toggleShowBooks() {
-			this.showBooks = !this.showBooks;
-		}
-	}
+  data() {
+    return {
+      showBooks: true,
+      title: "Harry Potter: Prisoner of Azkaban",
+      author: "J.K. Rowling"
+    }
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    }
+  }
 })
 
 app.mount('#app')
