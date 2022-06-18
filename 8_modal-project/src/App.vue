566effbb8c669f2928e75c06f5cd655804b52cd1
Anshul Verma <itsanshulverma@gmail.com>
2022-06-18 12:33:54 +0530

    v1

---
+++ 8_modal-project/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-show="showModal">
+    <Modal :title="header_title" :content="content" theme="bruh" @close="toggleModal"/>
+  </div>
+  <br />
+  <!-- Event Modifiers - alt -->
+  <button @click.alt="toggleModal">Open Modal (Alt + Click)</button>
+</template>
+
+<script>
+import Modal from './components/Modal'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: "My First Vue App",
+      header_title: "what up bruh! :)",
+      content: "Bruh, this is modal content.",
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  display: inline-block;
+  padding-bottom: 10px;
+  border-bottom: 2px solid #555;
+}
+</style>
---
+++ 8_modal-project/src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- Event Modifiers - self -->
+  <div class="backdrop"  @click.self="closeModal">
+    <div class="modal" :class="{ bruh: theme === 'bruh' }">
+      <h1 class="modal-title">{{ title }}</h1>
+      <p class="modal-content">{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+  props: ['title', 'content', 'theme'],
+  methods: {
+    closeModal() {
+      // Emitting custom events
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: grid;
+    place-items: center;
+    z-index: 1;
+  }
+  .modal {
+    padding: 20px;
+    background-color: #fff;
+    border-radius: 10px;
+    width: 400px;
+    height: 200px;
+    z-index: 2;
+  }
+  .modal-title {
+    font-size: 1.7rem;
+    margin-bottom: 10px;
+    padding-bottom: 5px;
+    border: none;
+  }
+  .bruh {
+    background-image: url('https://c.tenor.com/tqh4nwYSQj4AAAAS/kermit-kermit-the-frog.gif');
+    background-size: cover;
+    color: #fff;
+  }
+
+</style>
