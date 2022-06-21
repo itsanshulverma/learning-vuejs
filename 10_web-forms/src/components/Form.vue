2491dd13e01d310a4354218f31eaf2ee1e60f772
Anshul Verma <itsanshulverma@gmail.com>
2022-06-21 17:10:36 +0530

    starter files

---
+++ 10_web-forms/src/components/Form.vue
@@ -0,0 +1,42 @@
+<template>
+  <form>
+    <label for="email">Email</label>
+    <input type="email" name="email" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+}
+</script>
+
+<style>
+form {
+  max-width: 480px;
+  margin: 30px auto;
+  background-color: #fff;
+  padding: 25px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 4px #ccc;
+  text-align: left;
+}
+label {
+  color: #999;
+  display: inline-block;
+  margin: 25px 0 15px;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 0.6em;
+  letter-spacing: 1px;
+}
+input {
+  display: block;
+  width: 100%;
+  padding: 10px 10px;
+  color: #444;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ccc;
+}
+</style>
