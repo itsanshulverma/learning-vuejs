1ac0414b5536999a5c64aa0c1098ab728de92b41
Anshul Verma <itsanshulverma@gmail.com>
2022-06-22 11:03:12 +0530

    go back, forward or redirect explicitly

--- 11_vue-router/src/App.vue
+++ 11_vue-router/src/App.vue
@@ -4,8 +4,28 @@
     <router-link :to="{ name: 'about' }">About</router-link> | 
     <router-link :to="{ name: 'jobs' }">Jobs</router-link>
   </nav>
-  <router-view/>
+   <router-view/>
+
+  <button @click="back">Go Back</button>
+  <button @click="redirect">Redirect</button>
+  <button @click="forward">Go Forward</button>
 </template>
+
+<script>
+export default {
+  methods: {
+    back() {
+      this.$router.go(-1)
+    },
+    redirect() {
+      this.$router.push({ name: 'home' })
+    },
+    forward() {
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -14,11 +34,14 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  /* centering */
+  max-width: 720px;
+  margin: auto auto;
 }
 
 nav {
   padding: 30px;
-  margin-bottom: 50px;
+  margin-bottom: 20px;
 }
 
 nav a {
@@ -32,5 +55,20 @@ nav a {
 nav a.router-link-exact-active {
   color: #fff;
   background-color: #2c3e50;
+}
+
+button {
+  padding: 10px 30px;
+  margin: 20px  10px;
+  background-color: #ddd;
+  border-radius: 5px;
+  border: none;
+  color: #2c3e50;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #ccc;
 }
 </style>
