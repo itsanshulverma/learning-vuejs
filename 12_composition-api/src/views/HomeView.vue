f97183bdf375cbd53cebb6304eee20b1f7159205
Anshul Verma <itsanshulverma@gmail.com>
2022-06-24 09:27:17 +0530

    composition api basics

---
+++ 12_composition-api/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <h2>{{ name }} - {{ age }}</h2>
+    <button @click="handleClick">Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  /* Setup runs before mounted or created hooks. */
+  // setup() {
+  //   console.log('setup')
+  // },
+  // created() {
+  //   console.log('created')
+  // },
+  // mounted() {
+  //   console.log('mounted')
+  // }
+
+  /* One thing to note that the name and age are not reactive
+    values, while in data() function while using Options API,
+    these will be reactive.
+  */
+  setup() {
+    let name = 'dexter'
+    let age = '24'
+
+    const handleClick = () => {
+      console.log('button clicked')
+    }
+
+    return { name: name, age, handleClick }
+  }
+}
+</script>
