47294613a7e554eaf0f245cbebca429e3bfd39a0
Anshul Verma <itsanshulverma@gmail.com>
2022-06-24 10:08:42 +0530

    Adding reactivity using refs vs reactive

--- 12_composition-api/src/views/HomeView.vue
+++ 12_composition-api/src/views/HomeView.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p ref="p">{{ name }} - {{ age }}</p>
+    <!-- <p ref="p">{{ name }} - {{ age }}</p>
     <button @click="handleClick">Click me</button>
+    <input type="text" v-model="name" />
+    <button @click="age++">Increment age</button> -->
+    <h2>Refs</h2>
+    <p>{{ userOne.name }} - {{ userOne.age }}</p>
+    <button @click="updateUserOne">Update user one</button>
+    <h2>Reactive</h2>
+    <p>{{ userTwo.name }} - {{ userTwo.age }}</p>
+    <button @click="updateUserTwo">Update user two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -27,19 +35,53 @@ export default {
     these will be reactive.
   */
   setup() {
-    console.log(this) // undefined
+    // console.log(this) // undefined
 
-    const p = ref(null)
-    console.log(p.value) // undefined
+    // const p = ref(null)
+    // console.log(p.value) // undefined
 
-    let name = 'dexter'
-    let age = '24'
+    // let name = 'dexter'
+    // let age = 24
 
-    const handleClick = () => {
-      console.log(p, p.value) // <p>dexter - 24</p>
-    }
+    // const handleClick = () => {
+    //   console.log(p, p.value) // <p>dexter - 24</p>
+    //   name = 'new name' // no change
+    // }
 
-    return { name: name, age, handleClick, p }
+    /* ----------------------------------------------- */
+    // Using ref to make values reactive
+    // const name = ref('dexter')
+    // const age =  ref(24)
+
+    // const handleClick = () => {
+    //   name.value = 'mario'
+    //   age.value = 25
+    // }
+
+    // return { name: name, age, handleClick }
+    /* ----------------------------------------------- */
+
+    /* ----------------------------------------------------- */
+    // Refs vs Reactive
+    // const userOne = ref({ name: 'dexter', age: 24 })
+    // const updateUserOne = () => {
+    //   userOne.value.age = 29
+    // }
+
+    // const userTwo = reactive({ name: 'mario', age: 26 })
+    // const updateUserTwo = () => {
+    //   userTwo.age = 36
+    // }
+
+    // return { userOne, userTwo, updateUserOne, updateUserTwo }
+    
+    /*
+      Reactive doesn't make primitive type values reactive,
+      so using ref is preferred.
+      e.g. reactive('mario') can not be updated later on.
+    */
+   /* ------------------------------------------------------- */
+
   }
 }
 </script>
