3f02e57cf45da0f506f8583e31036b942681b29e
Anshul Verma <itsanshulverma@gmail.com>
2022-06-25 13:33:52 +0530

    starter setup files

---
+++ 13_demo-blog-project/src/components/BlogList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>List of Blogs</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ 13_demo-blog-project/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <BlogList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BlogList from '@/components/BlogList.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    BlogList
+  }
+}
+</script>
